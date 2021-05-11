711008c3db89cf2455b4a674aa102bef8f803df2
Ivanna <voloshchuk.ivanna@gmail.com>
2021-05-11 21:04:09 +0300

    done

--- test_UnitTests.py
+++ test_UnitTests.py
@@ -3,16 +3,6 @@ import requests
 from triangle import hyp
 
 
-class TestHyp(unittest.TestCase):
-    def test_zero_triangle(self):
-        r = hyp(0, 0)
-        self.assertEqual(0, r)
-
-    def test_3_4_triangle(self):
-        r = hyp(3, 4)
-        self.assertEqual(5, r)
-
-
 class TestBookCreate(unittest.TestCase):
     def setUp(self):
         self.base_url = "http://pulse-rest-testing.herokuapp.com/"
@@ -31,8 +21,27 @@ class TestBookCreate(unittest.TestCase):
         r = requests.delete(self.base_url + "books/" + str(self.book_id))
         print(r.status_code)
 
+class TestHyp(unittest.TestCase):
+    def test_zero_triangle(self):
+        r = hyp(0, 0)
+        self.assertEqual(0, r)
 
-
+    def test_3_4_triangle(self):
+        r = hyp(3, 4)
+        self.assertEqual(5, r)
 
 if __name__== "__main__":
-    unittest.main()
+    # unittest.main()
+    test1 = TestHyp('test_zero_triangle')
+    test2 = TestHyp('test_3_4_triangle')
+    test3 = TestHyp('test_3_4_triangle')
+    # result1 = test1.run()
+    # print(result1)
+    # result2 = test2.run()
+    # print(result2)
+
+    suite = unittest.TestSuite([test1, test2]) #обьеденить тесты в suite
+    suite.addTest(test3)
+    result = unittest.TestResult()
+    suite.run(result)
+    print(result)
